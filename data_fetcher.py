ae6f339862a281339fc019765e9f44aeea2e7544
Vlad-Sebastian Ionescu <ionescu.vlad1@gmail.com>
2022-01-22 17:20:43 +0200

    Parallel + logging of small files.

--- data_fetcher.py
+++ data_fetcher.py
@@ -2,7 +2,7 @@ import os.path
 
 import requests
 from bs4 import BeautifulSoup
-
+from joblib import Parallel, delayed
 
 class DataFetcher:
 
@@ -22,14 +22,23 @@ class DataFetcher:
                     .replace('catalog.html', nc_filename)\
                     .replace('/catalog/', '/fileServer/')
                 nc_file_data = requests.get(to_download)
-                with open(os.path.join(self.DATA_FOLDER, nc_filename), 'wb') as f:
+                full_path = os.path.join(self.DATA_FOLDER, nc_filename)
+                with open(full_path, 'wb') as f:
                     f.write(nc_file_data.content)
                 print('Saved:', nc_filename)
+                file_sz = os.path.getsize(full_path)
+                if file_sz / 1024 / 1024 < 3:
+                    with open('log.txt', 'a+') as f:
+                        f.write(f'{nc_filename} seems to be too small!\n')
+
 
     def get_month(self, url):
-        for td in self.__get_tds(url):
+
+        def handle_td(td):
             anchor = td.find('a')
             if anchor:
                 print(f'--- Getting {anchor["href"]}:')
                 day_url = url.replace('catalog.html', anchor['href'])
                 self.get_day(day_url)
+
+        Parallel(n_jobs=4)(delayed(handle_td)(td) for td in self.__get_tds(url))
